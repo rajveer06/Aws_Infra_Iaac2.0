96645aae1d8c025bd97e4cac88124b13d00a21b3
Rajveer Chouhan <rajveer@cloudthat.com>
2025-04-16 12:44:39 +0530

    updated root var file

--- variable.tf
+++ variable.tf
@@ -1,7 +1,23 @@
-variable "aws_region" {}
-variable "key_name" {}
-variable "instance_type" {}
-variable "ami_id" {}
-variable "bucket_name" {}
+variable "aws_region" {
+    type = string
+    description = "AWS region for all resources."
+}
+variable "key_name" {
+    type = string
+    description = "Key name for the EC2 instance"
+}
+variable "instance_type" {
+    type = string
+    description = "EC2 instance type"
+}
+variable "ami_id" {
+    type = string
+    description = "AMI ID for the EC2 instance"
+    default = null
+}
+variable "bucket_name" {
+    type = string
+    description = "S3 bucket name for the EC2 instance"
+}
   
 

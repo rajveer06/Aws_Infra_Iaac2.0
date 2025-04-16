099e234afb34d4394f4311120f572793eaa26cff
Rajveer Chouhan <rajveer@cloudthat.com>
2025-04-16 13:14:19 +0530

    updated var file

--- variables.tf
+++ variables.tf
@@ -1,35 +1,8 @@
-variable "aws_region" {
-    type = string
-    description = "AWS region for all resources."
-    default = "us-west-2"
-}
-variable "key_name" {
-    type = string
-    description = "Key name for the EC2 instance"
-    default = "GitHub-EC2-Key"
-}
-variable "instance_type" {
-    type = string
-    description = "EC2 instance type"
-    default = "t2.micro"
-}
-variable "ami_id" {
-    type = string
-    description = "AMI ID for the EC2 instance"
-}
-variable "bucket_name" {
-    type = string
-    description = "S3 bucket name for the EC2 instance"
-    default = null
-}
-variable "aws_access_key" {
-    type = string
-    description = "AWS access key for the EC2 instance"
-    default = null
-}
-variable "aws_secret_key" {
-    type = string
-    description = "AWS secret key for the EC2 instance"
-    default = null
-}
+variable "aws_region" {}
+variable "key_name" {}
+variable "instance_type" {}
+variable "ami_id"{}
+variable "bucket_name" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
